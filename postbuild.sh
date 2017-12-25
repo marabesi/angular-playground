65695b3673d15f2fb05062613a479478df893c7b
marabesi <matheus.marabesi@gmail.com>
2017-12-25 19:19:28 -0200

    post build action added

---
+++ postbuild.sh
@@ -0,0 +1,4 @@
+git init
+git add ./todo-list/dist
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
