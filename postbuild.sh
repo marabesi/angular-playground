d1c11c628555be59510540e1e9eef109dcbecfb1
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-12-26 18:52:08 -0200

    Update postbuild.sh
    removed $ from string

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,4 @@
 git init
 git add -f ./todo-list/dist
 git commit -m "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" gh-pages
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" gh-pages
