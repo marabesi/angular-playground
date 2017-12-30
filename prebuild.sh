0048c02328f32fb4ad6d83a777372e5cb07f2595
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-12-30 18:21:28 -0200

    Update prebuild.sh
    fixed travis ci variables

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "BUMP HELLO WORLD set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
-export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+echo "BUMP HELLO WORLD set up $GITHUB_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
+export REPO_URL="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 git branch -a
