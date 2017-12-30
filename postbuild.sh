71b4119264283a2c0d8d12ca55faf117efed91e3
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-12-30 18:10:18 -0200

    Update postbuild.sh
    changed how to push code when the build is completed

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,5 @@
 git init
+git remote add origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
 git add -f ./todo-list/dist
 git commit -m "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" gh-pages
+git push --force origin gh-pages
