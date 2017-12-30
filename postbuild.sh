136bbac436fa2b6fe1092965221a6fb1c7fb5b4c
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-12-30 18:13:57 -0200

    Update postbuild.sh
    removed origin from postbuild

--- postbuild.sh
+++ postbuild.sh
@@ -1,5 +1,5 @@
 git init
-git remote add origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
+#git remote add origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
 git add -f ./todo-list/dist
 git commit -m "Deploy to Github Pages"
 git push --force origin gh-pages
