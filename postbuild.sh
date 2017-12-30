7670c3c690245a4df3ef9d9cafbb1636ae2cbf0e
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-12-30 19:10:37 -0200

    Update postbuild.sh
    inverted git commands

--- postbuild.sh
+++ postbuild.sh
@@ -1,13 +1,13 @@
+echo 'Commiting...'
+git commit -m "Deploy to Github Pages"
+echo 'Done!'
+
 echo 'pulling from origin'
 git pull origin gh-pages -f
 echo 'Done!'
 
 echo 'adding files...'
 git add -f ./todo-list/dist
-echo 'Done!'
-
-echo 'Commiting...'
-git commit -m "Deploy to Github Pages"
 echo 'Done!'
 
 echo 'Pushing changes to repo...'
