b6886e6be8daf84f7546da2194d5b41ed3c578ef
marabesi <matheus.marabesi@gmail.com>
2019-09-08 22:45:19 +0200

    changed git command execution flow (travis ci)

--- postbuild.sh
+++ postbuild.sh
@@ -2,16 +2,16 @@ echo 'Resetting'
 git checkout --force .
 echo 'Done!'
 
+echo 'pulling from origin'
+git fetch --all
+echo 'Done!'
+
 echo 'adding files...'
 git add -f ./todo-list/dist
 echo 'Done!'
 
 echo 'Commiting...'
 git commit -m "Deploy to Github Pages"
-echo 'Done!'
-
-echo 'pulling from origin'
-git fetch --all
 echo 'Done!'
 
 echo 'Pushing changes to repo...'
